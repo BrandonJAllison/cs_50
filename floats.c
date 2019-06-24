bd3f33c908e41a0ed586f1ae1a6d381c4050a2f7
Brandon Allison <brandona8128@outlook.com>
2019-06-23 22:52:33 -0400

    floats.c

---
+++ floats.c
@@ -0,0 +1,17 @@
+//using floats in math operations
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    //prompt user for x
+    float x = get_float ("x: ");
+    
+    //prompt user for y
+    float y = get_float ("y: ");
+        
+        
+    printf("x / y, %f\n", x / y);
+ 
+}
